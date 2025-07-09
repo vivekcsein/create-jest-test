27f193cfe83d7a80f958a63af615da29c1b38d78
vivek <vivekcse.in@gmail.com>
2025-07-09 14:00:53 +0530

    simple testcase for sum added

---
+++ src/components/Homepage.test.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/dom";
+
+// simple test case
+const Sum = (a: number, b: number) => {
+  return a + b;
+};
+
+test("add 2+3 should be equal to 5", () => {
+  expect(Sum(2, 3)).toBe(5);
+});
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 };
 
 export default Homepage;
