c0a9b0facd4646237133874f57a8c8c293b98281
vivek <vivekcse.in@gmail.com>
2025-07-09 14:02:41 +0530

    ui test case run using describe

--- src/components/Homepage.test.tsx
+++ src/components/Homepage.test.tsx
@@ -1,11 +1,22 @@
 import "@testing-library/dom";
+import { render, screen } from "@testing-library/react";
+
+import Homepage from "./Homepage";
 
 // simple test case
-const Sum = (a: number, b: number) => {
-  return a + b;
-};
+// const Sum = (a: number, b: number) => {
+//   return a + b;
+// };
 
-test("add 2+3 should be equal to 5", () => {
-  expect(Sum(2, 3)).toBe(5);
-  expect(Sum(6, 3)).not.toBe(5);
+// test("add 2+3 should be equal to 5", () => {
+//   expect(Sum(2, 3)).toBe(5);
+//   expect(Sum(6, 3)).not.toBe(5);
+// });
+
+//UI test case
+describe("testing home component", () => {
+  it("render a heading", () => {
+    render(<Homepage />);
+    expect(screen.getByText("Hello World")).toBeInTheDocument();
+  });
 });
